d2a4347c02e85396fb81fae60f9fde3822b304ff
talicopanda <tales.scopinho@gmail.com>
2020-03-07 13:37:09 -0500

    Population

--- population.py
+++ population.py
@@ -6,15 +6,33 @@ class Population:
     """Creates a population based on mutation of a single element
     """
 
-    def __init__(self, parent: str):
+    def __init__(self, parent: str, pop_size: int):
         self.parent = parent
+        self.parent_len = len(parent)
+        self.pop_size = pop_size
+
+    def generate_pop(self):
 
 
-    def generate_string(length: int) -> str:
-        """Generates a random string of 'abcdefghijklmnopqrstuvwxyz ' with
-        length <length>."""
+    def first_string(self) -> str:
+        """Generates a random string of 'abcdefghijklmnopqrstuvwxyz '."""
         letters = string.ascii_lowercase + ' '
-        return ''.join(random.choice(letters) for i in range(length))
+        return ''.join(random.choice(letters) for _ in range(self.parent_len))
+
+    def mutate_string(self, characters = 1) -> str:
+        """Generates a mutation from parent"""
+        letters = string.ascii_lowercase + ' '
+        mutation = self.parent
+        previous_indexes = []
+        for _ in range(characters):
+            index = random.randint(0, self.string_len - 1)
+            while index in previous_indexes:
+                index = random.randint(0, self.string_len - 1)
+            previous_indexes.append(index)
+            mutation[index] = random.choice(letters)
+        return mutation
+
+
 
 
 if __name__ == '__main__':
