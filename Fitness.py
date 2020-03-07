6ca5ea99579ba5de77a0173370db5dbb77a49690
talicopanda <tales.scopinho@gmail.com>
2020-03-07 14:34:25 -0500

    To Fix

--- Fitness.py
+++ Fitness.py
@@ -1,59 +1,8 @@
 """Fitness model"""
-import string
-import doctest
+
 
 
 class fitness:
     ScoreForRightPlace: int
     ScoreForRightLetter: int
     goal: str
-
-    def __init__(self, goal: str):
-        self.scoreForRightPlace = 3
-        self.scoreForRightLetter = 1
-        self.goal = goal
-
-    def judge(self, word: string):
-        word.setScore((self.scoreForRightPlace(word.content) +
-                       self.scoreForRightLetter(word.content))
-                      / (self.scoreForRightLetter * len(word.content)
-                         + self.scoreForRightPlace * len(word.content)))
-
-    def score_for_right_place(self, word: str) -> int:
-        """
-
-        :param word:
-        :return:
-        >>> ftns = fitness("Hello hi")
-        >>> ftns.score_for_right_place("hello hi")
-        21
-        >>> ftns = fitness("    ")
-        >>> ftns.score_for_right_place("    ")
-        12
-        >>> ftns = fitness(" H h")
-        >>> ftns.score_for_right_place("    ")
-        6
-        """
-        score = 0
-        for i in range(len(word)):
-            if self.goal[i] == word[i]:
-                score += self.scoreForRightPlace
-        return score
-
-    def scoreForRightLetters(self, word: str) -> int:
-        error_for_each_letter = {}
-
-
-        for char in self.goal:
-            error_for_each_letter[char] = abs(self.goal.count(char) - word.count(char))
-
-        total_error = 0
-        for key in error_for_each_letter:
-            total_error += error_for_each_letter[key]
-
-
-        return max((len(self.goal) * self.scoreForRightLetter) - total_error, 0)
-
-if __name__ == "__main__":
-
-    doctest.testmod()
--- String.py
+++ String.py
@@ -1,19 +1,62 @@
 import random
 from typing import List
 import string
+import doctest
 
 class String:
     """Creates a String object"""
     content: str
     score: float
+    goal: str
 
     def __init__(self, content: str):
+        self.correct_place_score = 3
+        self.right_letter_score = 1
+        self.goal = goal
         self.content = content
         self.score = calc_score(content)
 
     def __str__(self) -> str:
         return f'self.content ({self.score})'
 
-    def calc_score(self, score: float):
-        self.score = score
+    def judge(self, word: string):
+        word.setScore((self.correct_place_score(word.content) +
+                       self.right_letter_score(word.content))
+                      / (self.right_letter_score * len(word.content)
+                         + self.correct_place_score * len(word.content)))
+
+    def score_for_right_place(self, word: str) -> int:
+        """
+        >>> ftns = fitness("Hello hi")
+        >>> ftns.score_for_right_place("hello hi")
+        21
+        >>> ftns = fitness("    ")
+        >>> ftns.score_for_right_place("    ")
+        12
+        >>> ftns = fitness(" H h")
+        >>> ftns.score_for_right_place("    ")
+        6
+        """
+        score = 0
+        for i in range(len(word)):
+            if self.goal[i] == word[i]:
+                score += self.correct_place_score
+        return score
+
+    def right_letters_score(self, word: str) -> int:
+        error_for_each_letter = {}
+
+        for char in self.goal:
+            error_for_each_letter[char] = abs(self.goal.count(char) - word.count(char))
+        total_error = 0
+        for key in error_for_each_letter:
+            total_error += error_for_each_letter[key]
+        return max((len(self.goal) * self.right_letter_score) - total_error, 0)
+
+    def calc_score(self, content: str):
+        pass
+
+if __name__ == "__main__":
+    doctest.testmod()
+
 
