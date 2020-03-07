b435a3d57fd6aa2a2c842fcd1a3383e677a6bb0d
talicopanda <tales.scopinho@gmail.com>
2020-03-07 15:07:50 -0500

    Almost done

--- string_discovery.py
+++ string_discovery.py
@@ -2,13 +2,16 @@ import random
 import string
 import random
 
+from Population import Population
+
+
 class Simulation:
     """Simulate biosphere"""
     goal: str
 
     def __init__(self, goal: str, pop_size: int):
         self.goal = goal
-        self.gen = 0
+        self.gen = 1
         self.pop_size = pop_size
 
 
@@ -17,13 +20,24 @@ class Simulation:
         letters = string.ascii_lowercase + ' '
         return ''.join(random.choice(letters) for _ in range(size))
 
-    def begin_sim(self, goal: str) -> None:
-        first = self.first_string(len(self.goal))
-        while()
+    def begin_sim(self) -> None:
+        current = self.first_string(len(self.goal))
+        while current != self.goal:
+            self.gen += 1
+            new_pop = Population(current, self.pop_size, self.gen)
+            current = new_pop.fittest()
+            print(current)
 
 if __name__ == '__main__':
     print('Enter a lower case sentence:')
-    inp = input()
-    while isinstance(inp, str):
+    goal = input()
+    while isinstance(goal, str):
         print('Invalid input, try again:')
-        inp = input()
+        goal = input()
+    print('Enter a population size:')
+    pop_size = input()
+    while isinstance(pop_size, int):
+        print('Invalid input, try again:')
+        pop_size = input()
+    my_sim = Simulation(goal, pop_size)
+    my_sim.begin_sim()
