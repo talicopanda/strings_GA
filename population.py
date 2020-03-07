9e541fadeef89dbf90c796d386205975fc98cf78
talicopanda <tales.scopinho@gmail.com>
2020-03-07 12:45:01 -0500

    Initial

--- population.py
+++ population.py
@@ -1,7 +1,16 @@
-"""Creates a population based on mutation of a single element
-"""
+import random
+import string
 
-def __init__(parent: str):
+class Population:
+    """Creates a population based on mutation of a single element
+    """
+
+    def __init__(self, parent: str):
+        self.parent = parent
 
 
-def generate_string(length: int):
+    def generate_string(length: int) -> str:
+        """Generates a random string of 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM
+        NOPQRSTUVWXYZ ' with length <length>."""
+        letters = string.ascii_letters + ' '
+        return ''.join(random.choice(letters) for i in range(stringLength))
